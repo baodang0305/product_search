cdb57fbff8717f0038458b463bdadf7aa6ee4977
Bao Dang <dxhbao@sk-global.biz>
2023-07-10 22:23:06 +0700

    Fix

--- prebuild.sh
+++ prebuild.sh
@@ -3,6 +3,8 @@
 # Install Python dependencies
 # pip install -r requirements.txt
 
+pip install urllib3==1.26.6 
+
 python -m venv .env
 source .env/bin/activate
 pip install -U pip setuptools wheel
